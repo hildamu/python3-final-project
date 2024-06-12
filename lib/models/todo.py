a305d38a691bf56c8af80377782c6deede785a13
hildamu <mukamihilda437@gmail.com>
2024-06-12 09:25:49 +0300

    main

--- lib/models/todo.py
+++ lib/models/todo.py
@@ -56,3 +56,16 @@ def delete_task(task_index):
         print("Invalid task index.")
     conn.close()
 
+def main():
+    create_table()
+
+    while True:
+        print("\nMenu:")
+        print("1. Add Task")
+        print("2. View Tasks")
+        print("3. Mark Task as Complete")
+        print("4. Delete Task")
+        print("5. Exit")
+        choice = input("Enter your choice: ")
+
+
