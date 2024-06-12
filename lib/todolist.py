4dedc7c1fff9fe15abe97baee8d03d53dda9af81
hildamu <mukamihilda437@gmail.com>
2024-06-12 09:12:46 +0300

    view tasks

--- lib/todolist.py
+++ lib/todolist.py
@@ -1,3 +1,12 @@
 def add_task(task, task_list):
     task_list.append({"task": task, "completed": False})
     print("Task added successfully!")
+
+def view_tasks(task_list):
+    if task_list:
+        print("Your to-do list:")
+        for index, task in enumerate(task_list, start=1):
+            status = "✓" if task["completed"] else " "
+            print(f"{index}. [{status}] {task['task']}")
+    else:
+        print("Your to-do list is empty.")
